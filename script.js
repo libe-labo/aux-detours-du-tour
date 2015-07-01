688a973e63bb0dc209a38befd8430b784a10e375
Paul Joannon <hello@pauljoannon.com>
2015-07-01 12:29:10 +0200

    use fittext.js to managa title font-size

--- script.js
+++ script.js
@@ -4,4 +4,6 @@ $(function() {
     $('#sections').fullpage({
         sectionSelector : '.main-content__sections__section'
     });
+
+    $('.main-content__sections__section__text--cover h1').fitText(0.5);
 });
